3d705945e87562e9d89ff7cbd0f9bc3542ece8d6
RahulKurudigi <105605410+RahulKurudigi@users.noreply.github.com>
2022-06-16 14:45:49 +0530

    Add files via upload

---
+++ ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Just Hard to code</h2>
+      <ul>
+        <li>FOod RS: 10</li>
+        <li>Petrol: 100</li>
+        <li>Movies RS: 200</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseItem;
